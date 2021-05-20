c75b0cd0eb0cdab0597d8e1c71c9d4d655c9e7d4
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-05-20 13:40:07 -0400

    Extend to 3 yr

--- applications/Jesper_2021/plot.R
+++ applications/Jesper_2021/plot.R
@@ -7,11 +7,12 @@ d01b <- read.table('temp_01_b.txt', header = FALSE, skip = 2)
 d02s <- read.table('temp_02_s.txt', header = FALSE, skip = 2)
 names(d01b) <- names(d02s) <- c('dos', 'doy', 'smass', 'depth', 'air', 'wall', 'floor', 'slurry')
 
-png('slurry_temp.png', height = 4, width = 5, units = 'in', res = 600)
+png('slurry_temp.png', height = 4, width = 6, units = 'in', res = 600)
   #par(mfrow = c(3, 1), mar = c(4, 4.5, 2, 1))
 
-  plot(slurry ~ dos, data = d02s, type = 'l', ylim = c(0, 32), xlab = 'Day of year', ylab = expression('Temperature'~(degree*C)), xaxt = 'n')
-  axis(1, at = c(0, 100, 200, 365, 465, 565, 730), labels = c(0, 100, 200, 365, 100, 200, 365))
+  plot(slurry ~ dos, data = d02s, type = 'l', ylim = c(0, 32), xlab = 'Day of year', ylab = expression('Temperature'~(degree*C)), xaxt = 'n', las = 1)
+  axis(1, at = c(c(0, 100, 200, 365), c(100, 200, 365) + 365, c(100, 200, 365) + 2*365), 
+       labels = c(0, 100, 200, 365, 100, 200, 365, 100, 200, 365), las = 3)
   lines(slurry ~ dos, data = d01b, type = 'l', col = 'red')
   lines(air ~ dos, data = d02s, type = 'l', col = 'gray45', lty = 2)
   grid()
