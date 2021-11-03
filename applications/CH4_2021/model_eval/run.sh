1c3160473b162dceb1c2b7d24b775d808dc992da
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-11-03 17:46:23 -0400

    Use meas air temperature for model eval

--- applications/CH4_2021/model_eval/run.sh
+++ applications/CH4_2021/model_eval/run.sh
@@ -3,21 +3,24 @@
 sim_start_time="$SECONDS"
 
 # Run all simulations in parallel
-./stm Back pars/pars.txt pars/Back_user_pars.txt &
-./stm Fitt pars/pars.txt pars/Fitt_user_pars.txt &
-./stm Lind pars/pars.txt pars/Lind_user_pars.txt &
-./stm Raan pars/pars.txt pars/Raan_user_pars.txt
+./stm Back pars/pars.txt pars/Back_user_pars.txt weather/weather.txt&
+./stm Fitt pars/pars.txt pars/Fitt_user_pars.txt weather/weather.txt &
+./stm Lind pars/pars.txt pars/Lind_user_pars.txt weather/weather.txt &
+./stm Raan pars/pars.txt pars/Raan_user_pars.txt weather/weather.txt
 
 sim_end_time="$SECONDS"
 
 # Move output
-mv *temp.txt* stm_output
-mv *weather* stm_output
-mv *rates* stm_output
+mv *_temp.txt* stm_output
+mv *_weather* stm_output
+mv *_rates* stm_output
 
 # Run R scripts
 
 R CMD BATCH --nosave --norestore 'plot.R'
+
+rm Rplots.pdf
+rm plot.Rout
 
 plot_end_time="$SECONDS"
 
