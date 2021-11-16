e69e21eb7729327bf04a39e02719c445cb14cd12
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-11-16 05:49:51 -0500

    Pick one year for Uppsala application

--- applications/CH4_2021/Uppsala_weather/scripts/doy_summ.R
+++ applications/CH4_2021/Uppsala_weather/scripts/doy_summ.R
@@ -1,4 +1,6 @@
 # Prepare Uppsala weather data for input in stm model
+# Note that model currently accepts one obs per DOY
+# Measurements have some 2020 and 2021 values for May, so this is a bit of a problem
 
 library(lubridate)
 source('../functions/rounddf.R')
@@ -12,10 +14,17 @@ dat$doy <- as.integer(as.character(dat$date.time, format = '%j'))
 dat$year <- as.integer(as.character(dat$date.time, format = '%Y'))
 dat$date <- date(dat$date.time)
 
-# year and date not used now
+# Subset
+dat <- subset(dat$date >= ymd('2020 05 01') & dat$date <= ymd('2021 05 31'))
 
 # Summarize by doy
-datd <- aggregate(dat[, c('temp.1.5m', 'rad')], dat[, c('doy'), drop = FALSE], mean)
+mns <- aggregate(dat[, c('temp.1.5m', 'rad')], dat[, c('doy'), drop = FALSE], mean)
+yrs <- aggregate(dat[, c('year')], dat[, c('doy'), drop = FALSE], function(x) x[1])
+ns <- aggregate(dat[, c('temp.1.5m')], dat[, c('doy'), drop = FALSE], function(x) length(x))
+datd <- merge(yrs, mns, by = 'doy')
 datd <- rounddf(datd, 3, signif)
 
-write.table(datd, '../output/weather.txt', row.names = FALSE)
+# Check n
+if (any(ns$n > 24)) stop('Count error')
+
+write.table(datd, '../output/Uppsala_doy_weather.txt', row.names = FALSE)
