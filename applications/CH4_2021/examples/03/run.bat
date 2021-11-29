cf03e28a84394f203c8c2336a407e9514d001832
Sasha Hafner <35272876+sashahafner@users.noreply.github.com>
2021-11-29 12:47:10 -0500

    Info on run by double-click
    Plus resulting output

--- applications/CH4_2021/examples/03/run.bat
+++ applications/CH4_2021/examples/03/run.bat
@@ -1,4 +1,5 @@
 REM Batch file to run example 1 on Windows
 REM This example uses calculated weather and calculated slurry level based on a fixed addition rate and 2 removal events per year
+REM Note that the user parameter file name has been changed to the default of user_pars.txt--this was done so the example can be run by double-clicking stm.exe instead of using the command below or this script (but the ID 0001 will be used for output files)
 
-.\stm.exe 0003 pars.txt Back_u_pars.txt
+.\stm.exe 0003 pars.txt user_pars.txt
