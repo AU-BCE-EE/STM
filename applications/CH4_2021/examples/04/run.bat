c2777c9681d825c1248d09b9181fe22c48fda27a
Sasha Hafner <35272876+sashahafner@users.noreply.github.com>
2021-11-30 07:21:33 -0500

    Compile Win version, update ex 04
    Has level fix.

--- applications/CH4_2021/examples/04/run.bat
+++ applications/CH4_2021/examples/04/run.bat
@@ -1,4 +1,4 @@
-REM Batch file to run example 1 on Windows
+REM Batch file to run example 4 on Windows
 REM This example uses measured weather and slurry level
 
-.\stm.exe 0004 pars.txt user_pars.txt weather.txt Back_level.txt
+.\stm.exe 0004 pars.txt user_pars.txt weather.txt level_sop.txt
