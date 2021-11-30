dce050f7113321972e5e358c06da2b1049520683
Sasha D. Hafner <sasha.hafner@gmail.com>
2021-11-30 07:13:36 -0500

    Add SOP example

---
+++ applications/CH4_2021/examples/04/run.bat
@@ -0,0 +1,4 @@
+REM Batch file to run example 1 on Windows
+REM This example uses measured weather and slurry level
+
+.\stm.exe 0004 pars.txt user_pars.txt weather.txt Back_level.txt
---
+++ applications/CH4_2021/examples/04/run.sh
@@ -0,0 +1,4 @@
+# Batch file to run example 1 on Windows
+# This example uses measured weather and slurry level
+
+./stm 0004 pars.txt user_pars.txt weather.txt level_sop.txt
