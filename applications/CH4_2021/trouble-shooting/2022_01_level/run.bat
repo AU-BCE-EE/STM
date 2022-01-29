68d543decb5857b5fb6685441911962f36c5d2a6
Sasha Hafner <35272876+sashahafner@users.noreply.github.com>
2022-01-29 07:06:08 -0500

    Add input files from SOP
    Includes Tjele weather file (with data)

--- applications/CH4_2021/trouble-shooting/2022_01_level/run.bat
+++ applications/CH4_2021/trouble-shooting/2022_01_level/run.bat
@@ -1,3 +1,4 @@
-REM Batch file to run problematic simulation on Windows
+REM Batch file to run example 1 on Windows
+REM This example uses measured weather but calculated slurry level based on a fixed addition rate and 2 removal events per year
 
-.\stm.exe DK01 pars.txt DK_Back_u_pars.txt Tjele_weather.txt DK_level.txt
+.\stm.exe DK pars.txt DK_Back_u_pars.txt Tjele_weather.txt DK_level.txt
