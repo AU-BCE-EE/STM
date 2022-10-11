8391453e4d3e296e522a8eea39bbb452b8a2ec56
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-10-11 11:46:49 -0400

    Compile local ver

--- src/compile.sh
+++ src/compile.sh
@@ -1,7 +1,7 @@
 # Compile stm.f90 on Linux
 
 gfortran stm.f90 -o stm
-cp stm ../tests/stm
+#cp stm ../tests/stm
 mv stm ../bin/Linux/stm
 
 echo 'Compiled stm (or tried to). Did you update version number in stm.f90?'
